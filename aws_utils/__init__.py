aa2ffb5acdf15c21e54a09cbb5cafc58e0e9d1dc
Rutger de Graaf <rutger@whizzkit.nl>
2023-02-28 15:35:11 +0100

    Update __init__.py

--- aws_utils/__init__.py
+++ aws_utils/__init__.py
@@ -3,6 +3,7 @@ from typing import Union
 import string
 import re
 import boto3
+from configparser import ConfigParser
 
 
 def assume_role(
@@ -48,3 +49,48 @@ export AWS_SESSION_TOKEN={credentials["SessionToken"]}'''
         return credentials
 
     raise Exception(f'No way to handle output {output}')
+
+
+def load_config(file):
+    config_parser = ConfigParser()
+    if len(config_parser.read(file)) != 1:
+        raise Exception(f'Could not read file {file}')
+    return config_parser
+
+
+def get_session(access_key_id, secret_access_key, region_name):
+    return boto3.Session(
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        region_name=region_name
+    )
+
+
+def create_access_key(access_key_id, secret_access_key, region_name, user_name):
+    client = get_session(access_key_id, secret_access_key, region_name).client('iam')
+    deleted_access_keys = []
+    for access_key in [
+        k for k in client.list_access_keys(UserName=user_name)['AccessKeyMetadata'] if k['Status'] == 'Inactive'
+    ]:
+        inactive_access_key_id = access_key['AccessKeyId']
+        client.delete_access_key(
+            UserName=access_key['UserName'],
+            AccessKeyId=access_key['AccessKeyId']
+        )
+        deleted_access_keys.append(inactive_access_key_id)
+    return client.create_access_key(UserName=user_name)['AccessKey'], deleted_access_keys
+
+
+def deactivate_access_key(access_key_id, secret_access_key, region_name, user_name, old_access_key_id):
+    get_session(access_key_id, secret_access_key, region_name).client('iam').update_access_key(
+        UserName=user_name,
+        AccessKeyId=old_access_key_id,
+        Status='Inactive'
+    )
+
+
+def delete_access_key(access_key_id, secret_access_key, region_name, user_name, old_access_key_id):
+    get_session(access_key_id, secret_access_key, region_name).client('iam').delete_access_key(
+        UserName=user_name,
+        AccessKeyId=old_access_key_id
+    )
